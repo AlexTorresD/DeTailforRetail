c58b6c75e56d5d628a9e56536b07403aedfd856b
Josh Yang <330909261y@gmail.com>
2022-07-30 15:55:24 -0500

    finished CRUD functionatlities for Manufacturer entity

--- app.py
+++ app.py
@@ -14,10 +14,214 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'secret string'
 
 db = SQLAlchemy(app)
+    
+class Store(db.Model): 
+ Store_ID = db.Column(db.Integer, primary_key=True)
+ Store_Name = db.Column(db.String(128))
+ Location = db.Column(db.String(128))
+ Order_Relation = db.relationship('Orders', backref='store', lazy=True)
+ 
+class Employee(db.Model):
+ Employee_ID = db.Column(db.Integer, primary_key=True)
+ Employee_Fname = db.Column(db.String(64))
+ Employee_Lname = db.Column(db.String(64))
+ Employee_Email = db.Column(db.String(128))
+ Employee_Phone = db.Column(db.String(128))
+ Position = db.Column(db.String(128))
+ Hours_Worked = db.Column(db.Integer)
+ Salary = db.Column(db.Float)
+ 
+class Manufacturer(db.Model):
+ Manufacturer_ID = db.Column(db.Integer, primary_key=True)
+ Manufacturer_Name = db.Column(db.String(128))
+ Manufacturer_Email = db.Column(db.String(128))
+ Manufacturer_Phone = db.Column(db.String(128))
+ Manufacturer_Headquarters = db.Column(db.String(128))
+ Manufacturer_Description = db.Column(db.String(128))
+ Product_Relation = db.relationship('Product', backref='manufacturer', lazy=True)
+ 
+class Product(db.Model):
+ Product_ID = db.Column(db.Integer, primary_key=True)
+ Manufacturer_ID = db.Column(db.Integer, db.ForeignKey('manufacturer.Manufacturer_ID')) #foreign ref
+ Product_Price = db.Column(db.Float)
+ Product_Quantity = db.Column(db.Integer)
+ Product_Size = db.Column(db.Integer)
+ Product_Type = db.Column(db.String(128))
+ Product_Description = db.Column(db.String(128))
+ Orders_Relation = db.relationship('Orders', backref='product', lazy=True)
+ 
+class Orders(db.Model):
+ Order_ID = db.Column(db.Integer, primary_key=True)
+ Store_ID = db.Column(db.Integer, db.ForeignKey('store.Store_ID')) #foreign ref
+ Product_ID = db.Column(db.Integer, db.ForeignKey('product.Product_ID')) #foreign ref
+ Order_Quantity = db.Column(db.Integer)
+ Order_Price = db.Column(db.Float)
+ Order_Date = db.Column(db.DateTime)
+ Received = db.Column(db.Boolean, default=False, nullable=False)
+ 
+class Staff(db.Model):
+ Staff_ID = db.Column(db.Integer, primary_key=True)
+ Store_ID = db.Column(db.Integer, db.ForeignKey('store.Store_ID'))
+ Employee_ID = db.Column(db.Integer, db.ForeignKey('employee.Employee_ID'))
+ Employee_Relation = db.relationship('Employee', backref='staff', lazy=True)
+
+if __name__ == '__main__':
+    app.run()
+
+def getManf():
+    query = select(Manufacturer)
+    result = db.session.execute(query)
+    
+    manf_list = []
+    for manf in result.scalars():
+        manf_list.append((manf.manufacturer_id, manf.manufacturer_name, manf.manufacturer_email,
+                          manf.manufacturer_phone, manf.manufacturer_headquarters, manf.manufacturer_description, manf.product_relation))
+    return manf_list;
 
 
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+##### CREATE
+
+@app.route("/createManf")
+def createManf(feedback_message=None, feedback_type=False):
+    return render_template("", feedback_message=feedback_message,
+                           feedback_type=feedback_type)
+    ##### insert url
+
+@app.rout("/manfCreate", methods=['POST'])
+def manfCreate():
+    ####form[<-- name of HTML element]
+    id = request.form["manf_id"]
+    name = request.form["manf_name"]
+    email = request.form["manf_email"]
+    phone = request.form["manf_phone"]
+    headquarters = request.form["manf_headquarters"]
+    desc = request.form["manf_description"]
+    
+    try:
+        entry = Manufacturer(manufacturer_id = id, manufacturer_name = name,
+                             manufacturer_email = email, manufacturer_phone = phone,
+                             manufacturer_headquarters = headquarters, 
+                             manufacturer_description = desc)
+    except exc.IntegrityError as err:
+        db.session.rollback()
+        return createManf(feedback_message='A Manufacturer named {} already exists. Create a Manufacturer with a different name.'.format(name), feedback_type=False)
+    except Exception as err:
+        db.session.rollback()
+        return createManf(feedback_message='Database error: {}'.format(err), feedback_type=False)
+    
+    return createManf(feedback_message='Successfully added Manufacturer {}'.format(name), ##### might need to change name
+                       feedback_type=True)
+    
+def get_manf(manfName):
+    query = select(Manufacturer).where(Manufacturer.Manufacturer_Name == manfName)
+    result = db.session.execute(query)
+    manf = result.scalar()
+    if manf is None:
+        raise('Manufacturer not found')
+    return manf
+
+## maby add get_manf_from_id function
+
+
+##### READ
+@app.route("/readManf")
+def readManf():
+    query = select(Manufacturer)
+    result = db.session.execute(query)
+    
+    manf_list = []
+    for manf in result.scalars(): ####may need to not capitalize here
+        manf_list.append((manf.manufacturer_id, manf.manufacturer_name, manf.manufacturer_email,
+                          manf.manufacturer_phone, manf.manufacturer_headquarters, manf.manufacturer_description))
+        
+    return render_template("readManf.html", manflist=manf_list)
+    
+
+##### UPDATE
+@app.route("/update") #####may needto fix this part
+def updatemanf(feedback_message=None, feedback_type=False):
+    manf_names = [name for _, name, _, _, _, _ in getManf()]
+    return render_template("updateManf.html",
+                           manfnames = manf_names,
+                           feedback_message=feedback_message,
+                           feedback_type=feedback_type)
+
+@app.route("/manfupdate", methods=['POST'])
+def manfupdate():
+    manf_name = request.form.get('manfnames')
+    name = request.form["name"]
+    hq = request.form["hq"]
+    email = request.form["email"]
+    phone= request.form["phone"]
+    desc = request.form["desc"]
+    
+    try:
+        obj = db.session.query(Manufacturer).filter(
+            Manufacturer.Manufacturer_Name == manf.name).first()
+        
+        if obj == None:
+            msg = 'Manufacturer {} not found.'.format(manf_name)
+            return updatemanf(feedback_message=msg, feedback_type=False)
+        
+        ##may need to capitalize here
+        if name != '':
+            obj.manufacturer_name = name
+        if hq != '':
+            obj.manufacturer_headquarters = hq
+        if email != '':
+            obj.manufacturer_email = email
+        if phone != '':
+            obj.manufacturer_description = desc
+            
+        db.session.commit()
+        
+    except Exception as err:
+        db.session.rollback()
+        return updatechef(feedback_message=err, feedback_type=False)
+
+    return updatechef(feedback_message='Successfully updated chef {}'.format(manf_name),
+                       feedback_type=True)
+        
+##### DELETE
+@app.route("/deleteManf")
+def deleteManf(feedback_message=None, feedback_type=False):
+    manf_names = [name for _, name, _, _, _, _ in getManf()]
+    return render_template("deleteManf.html", manfnames = manf_names,
+                           feedback_type=feedback_message,
+                           feedback_type=feedback_type)
+
+@app.route("/manfDelete", methods=['POST'])
+def manfDelete():
+    if not request.form.get('confirmInput'):
+        return deleteManf(feedback_message='Operation canceled. Manufacturer not deleted', feedback_type=False)
+    
+    manf_name = request.form.get('manfnames')
+    
+    try:
+        obj = db.session.query(Manufacturer).filter(
+            Manufacturer.Manufacturer_Name == manf_name).filter()
+        
+        if obj == None:
+            msg = 'Manufacturer {} not found.'.format(manf_name)
+            return deleteManf(feedback_message=msg, feedback_type=False)
+        
+        db.session.delete(obj)
+        db.session.commit()
+    except Exception as err:
+        db.session.rollback()
+        return deleteManf(feedback_message=err, feedback_type=False)
+    
+    return deleteManf(feedback_message='Successfully deleted Manufacturer {}'.format(manf_name),
+                      feedback_type=True)
+    
 
 '''
+
+    
 class Dish(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     dname = db.Column(db.String(64), unique=True)
@@ -41,66 +245,6 @@ class ChefInfo(db.Model):
     addr = db.Column(db.String(128))
     phone = db.Column(db.String(64))
     
-
-'''
-class Store(db.Model):  
-    Store_ID = db.Column(db.Integer, primary_key=True)
-    Store_Name = db.Column(db.String(128))
-    Location = db.Column(db.String(128))
-    product_Relation = db.relationship('product', backref='store', lazy=True)
-    
-class Employee(db.Model):
-    Employee_ID = db.Column(db.Integer, primary_key=True)
-    Store_ID = db.Column(db.Integer, db.ForeignKey('store.Store_ID')) #add foreign key
-    Employee_Fname = db.Column(db.String(64))
-    Employee_Lname = db.Column(db.String(64))
-    Employee_Email = db.Column(db.String(128))
-    Employee_Phone = db.Column(db.String(128))
-    Position = db.Column(db.String(128))
-    Hours_Worked = db.Column(db.Integer)
-    Salary = db.Column(db.Float)
-
-class Manufacturer(db.Model):
-    Manufacturer_ID = db.Column(db.Integer, primary_key=True)
-    Manufacturer_Name = db.Column(db.String(128))
-    Customer_Email = db.Column(db.String(128))
-    Customer_Phone = db.Column(db.String(128))
-    Manufacturer_Headquarters = db.Column(db.String(128))
-    Manufacturer_Description = db.Column(db.String(128))
-    product_Relation = db.relationship('product', backref='manufacturer', lazy=True)
-
-class Product(db.Model):
-    Product_ID = db.Column(db.Integer, primary_key=True)
-    Manufacturer_ID = db.Column(db.Integer, db.ForeignKey('manufacturer.Manufacturer_ID')) #foreign ref
-    Product_Price = db.Column(db.Float)
-    Product_Quantity = db.Column(db.Integer)
-    Product_Size = db.Column(db.Integer)
-    Product_Type = db.Column(db.String(128))
-    Product_Description = db.Column(db.String(128))
-    ordes_Relation = db.relationship('orders', backref='product', lazy=True)
-
-class Orders(db.Model):
-    Order_ID = db.Column(db.Integer, primary_key=True)
-    Store_ID = db.Column(db.Integer, db.ForeignKey('store.Store_ID')) #foreign ref
-    Product_ID = db.Column(db.Integer, db.ForeignKey('product.Product_ID')) #foreign ref
-    Order_Quantity = db.Column(db.Integer)
-    Order_Price = db.Column(db.Float)
-    Order_Date = db.Column(db.DateTime)
-    Received = db.Column(db.Boolean)
-
-
-class Staff(db.Model):
-    Staff_ID = db.Column(db.Integer, primary_key=True)
-    Store_ID = db.Column(db.Integer, db.ForeignKey('store.Store_ID'))
-    Employee_ID = db.Column(db.Integer, db.ForeignKey('employee.Employee_ID'))
-    employee_Relation = db.relationship('employee', backref='staff', lazy=True)
-    
-
-if __name__ == '__main__':
-    app.run()
-
-
-'''
 def getchefs():
     query = select(ChefInfo)
     result = db.session.execute(query)
@@ -135,13 +279,9 @@ def getcooks():
 
 
     return cookslist
-
-
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-
+    
+    
+    
 ##### CREATE 
 
 @app.route("/createchef")
@@ -155,7 +295,6 @@ def chefcreate():
     cname = request.form["cname"]
     addr = request.form["addr"]
     phone = request.form["phone"]
-
     try:
         entry = ChefInfo(cname=cname, addr=addr, phone=phone)
         db.session.add(entry)
@@ -511,4 +650,5 @@ def cooksdelete():
 
     return deletecooks(feedback_message='Successfully deleted cooks relationship between {} and {}'.format(chef_name, dish_name),
                        feedback_type=True)
+
 '''
